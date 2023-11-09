cbdfab74c4840d655ec97ddde0becbc10bf5f2fa
SkyzerFlyzer <skyzerflyzer@gmail.com>
2023-11-09 08:38:13 +0000

    Optimised regex pattern creation

--- daily challenges/2023/november/day_9/day_9.py
+++ daily challenges/2023/november/day_9/day_9.py
@@ -3,7 +3,8 @@ import re
 
 class Solution:
     def countHomogenous(self, s: str) -> int:
-        homogenous_substrings = [x[0] for x in re.findall(r'((.)\2*)', s)]
+        pattern = re.compile(r'((.)\2*)')
+        homogenous_substrings = [x[0] for x in re.findall(pattern, s)]
         total = 0
         for substring in homogenous_substrings:
             total += len(substring) * (len(substring) + 1) // 2
