fda576535bb484f742704fd729ad701d43f8dc4f
SkyzerFlyzer <skyzerflyzer@gmail.com>
2023-11-09 08:48:49 +0000

    Small optimisation for itertools solution

--- daily challenges/2023/november/day_9/day_9.py
+++ daily challenges/2023/november/day_9/day_9.py
@@ -4,9 +4,8 @@ import re
 
 class Solution:
     def countHomogenous(self, s: str) -> int:
-        homogenous_substrings = itertools.groupby(s)
         total = 0
-        for _, group in homogenous_substrings:
+        for _, group in itertools.groupby(s):
             group_len = len(list(group))
             total += (group_len * (group_len + 1)) // 2
         return total % (10**9 + 7)
