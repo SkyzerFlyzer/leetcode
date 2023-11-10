d4f00022b29987778c21657e7b2c17074ad60b50
SkyzerFlyzer <skyzerflyzer@gmail.com>
2023-11-10 09:04:06 +0000

    Optimised day_10 solution

--- daily challenges/2023/november/day_10/day_10.py
+++ daily challenges/2023/november/day_10/day_10.py
@@ -5,10 +5,10 @@ from typing import List
 
 class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
-        graph = {}
+        graph = collections.defaultdict(list)
         for a, b in adjacentPairs:
-            graph.setdefault(a, []).append(b)
-            graph.setdefault(b, []).append(a)
+            graph[a].append(b)
+            graph[b].append(a)
         sorted_graph = sorted(graph.items(), key=lambda x: len(x[1]))
         start, _ = sorted_graph[0]
         end, _ = sorted_graph[1]
